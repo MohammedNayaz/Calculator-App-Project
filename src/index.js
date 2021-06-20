d20b4ba5b308f4ffdef51bfced2f610701ad79c0
Mohammed Nayaz <mohammednayaz493@gmail.com>
2021-06-20 06:57:53 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Calculator from './Calculator';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
